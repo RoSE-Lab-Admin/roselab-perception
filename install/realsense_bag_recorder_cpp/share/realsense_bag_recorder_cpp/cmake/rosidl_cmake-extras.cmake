af7a970150cf578481c592581fb3919b07b85931
Ryan Hartzell <ryan_hartzell@mines.edu>
2025-10-05 16:50:44 -0600

    removing all the log and installs that got committed... :|

--- install/realsense_bag_recorder_cpp/share/realsense_bag_recorder_cpp/cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(realsense_bag_recorder_cpp_IDL_FILES "srv/RecordBag.idl")
-set(realsense_bag_recorder_cpp_INTERFACE_FILES "srv/RecordBag.srv")
