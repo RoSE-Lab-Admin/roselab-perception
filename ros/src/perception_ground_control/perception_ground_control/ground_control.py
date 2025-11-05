03a1026b8acefe597b4a06981c237dd7805b6969
Lexi Hanlon <lexi.j.hanlon@gmail.com>
2025-11-05 15:10:12 -0700

    AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH

--- ros/src/perception_ground_control/perception_ground_control/ground_control.py
+++ ros/src/perception_ground_control/perception_ground_control/ground_control.py
@@ -152,7 +152,7 @@ class groundcontrol(Node):
         capture_request.outname = self.pi_file
         capture_request.duration = 60.0 # dummy val
         self.cap_future = self.mast_start.call_async(capture_request)
-        rclpy.spin_until_future_complete(self,self.cap_future)
+        
 
         self.get_logger().info("Mastcam capture started")
 
