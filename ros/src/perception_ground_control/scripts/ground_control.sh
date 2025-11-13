52e42abdbc46edc8c7c95fbeaa69c8ce69a6ecf1
Ryan Hartzell <ryan_hartzell@mines.edu>
2025-11-13 12:13:45 -0700

    work change

--- ros/src/perception_ground_control/scripts/ground_control.sh
+++ ros/src/perception_ground_control/scripts/ground_control.sh
@@ -1,31 +1,42 @@
 #!/usr/bin/env bash
 # configurable delay after LiDAR start
 
-LIDAR_SECS=60
+LIDAR_SECS=30 # Change depending on trial scan length
 
 echo "Perception Ground Control Started!"
 
-while true; do
-    # 1. start LiDAR
-    read -r -p "Press Enter to START LiDAR (/start_lidar)..." _
-    echo "Starting LiDAR..."
-    ros2 topic pub --once /start_lidar std_msgs/msgs/Bool "{data: true}"
+# 1. start LiDAR
+read -r -p "Press Enter to START LiDAR (/start_lidar)..."
+echo "Starting LiDAR..."
+ros2 topic pub --once /start_lidar std_msgs/msg/Bool "{data: true}"
 
-    # 2. spinup / fixed wait
+# 2. spinup / fixed wait
+echo "Waiting ${LIDAR_SECS}s for LiDAR to finish..."
+sleep "${LIDAR_SECS}"
+
+while true; do
+    # 3. start MastCam + Rosey bag
+    read -r -p "Press Enter to START MastCam (/start_mastcam) AND RoSEy bag (/start_rosey_bag)..."
+    echo "Starting MastCam..."
+    ros2 topic pub --once /start_mastcam std_msgs/msg/Bool "{data: true}"
+
+    # 4. stop both
+    read -r -p "Press Enter to STOP MastCam (/stop_mastcam) AND Rosey bag (/stop_rosey_bag)..."
+    echo "Stopping MastCam..."
+    ros2 topic pub --once /stop_mastcam std_msgs/msg/Bool "{data: true}"
+
+    # 5. start LiDAR
+    read -r -p "Press Enter to START LiDAR (/start_lidar)..."
+    echo "Starting LiDAR..."
+    ros2 topic pub --once /start_lidar std_msgs/msg/Bool "{data: true}"
+
+    # 6. spinup / fixed wait
     echo "Waiting ${LIDAR_SECS}s for LiDAR to finish..."
     sleep "${LIDAR_SECS}"
 
-    # 3. start MastCam + Rosey bag
-    read -r -p "Press Enter to START MastCam (/start_mastcam) AND RoSEy bag (/start_rosey_bag)..." _
-    echo "Starting MastCam..."
-    ros2 topic pub --once /start_mastcam std_msgs/msgs/Bool "{data: true}"
-
-    # 4. stop both
-    read -r -p "Press Enter to STOP MastCam (/stop_mastcam) AND Rosey bag (/stop_rosey_bag)..." _
-    echo "Stopping MastCam..."
-    ros2 topic pub --once /stop_mastcam std_msgs/msgs/Bool "{data: true}"
-
     echo
-    echo "Cycle complete. Ctrl+C to exit, or it will start over."
+    echo "Cycle complete. Ctrl+C to exit, or it will start over. Waiting 10 seconds for input..."
+    sleep 10
     echo
+
 done
